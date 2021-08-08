0b41e435a293a4e13bd20c9b7f30dc4f66229147
Ivaylo Banchevski <ivaylo.banchevski@gmail.com>
2021-08-08 15:03:52 +0000

    Introduce admin registration
    Admins can be registered and tokens can be validated.

---
+++ server/controllers/AdminController.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+
+module.exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).send({"status": "error", "error": 'Invalid request!'});
+  }
+
+  res.status(201).send('registered successfully');
+};
+
+module.exports.login = function (req, res) {
+  const { email, password } = req.body;
+
+  
+  if (!email || !password) {
+    return res.status(400).send({"status": "error", "error": 'Invalid request!'});
+  }
+
+  // Database check
+  if (email !== 'admin@admin') {
+    return res.status(400).send({"status": 'error', "error": 'Invalid email or password!'});
+  }
+
+  // Everything is ok, generate token
+  const tokenPayload = {
+    "email": email,
+  };
+  const token = jwt.sign(tokenPayload, process.env.TOKEN_SECRET);
+  console.log(`signed token for user: ${token}`);
+
+  res.json({"status": 'success', "message": 'Successfully authorized!', "token": token});
+};
