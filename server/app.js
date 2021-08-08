c6a8de09e494dc3b2b61644409b7d10f4f6d9a24
Ivaylo Banchevski <ivaylo.banchevski@gmail.com>
2021-08-08 14:56:11 +0000

    Add cookieparser and configure dotenv
    Initial cookieparser and dotenv configurations added here
    there is no functionallity yet in this commit.

--- server/app.js
+++ server/app.js
@@ -1,16 +1,27 @@
-const express = require("express");
+const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const adminRoutes = require('./routes/admin');
+
+// Init dotenv
+require('dotenv').config();
+
+
 // Middleware
-app.use(express.json());
+app.use(cookieParser());
+app.use(express.json({ extended: true }));
 
 // Routes
 app.get('/', (req, res) => {
     res.send("Raboti");
 });
+
+app.use('/apiv1', adminRoutes);
+
 
 // Server
 app.listen(PORT, () => {
