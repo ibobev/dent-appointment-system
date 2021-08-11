f2fede92879197733b193e7713c1a42bf5752bbb
Ivan Bobev <bobev.ivan99@gmail.com>
2021-08-12 00:36:07 +0300

    Added images to register cards

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,46 +1,80 @@
 <template>
   <main>
     <section class="pt-4" id="home">
-        <h1>The Dentist Appointment App</h1>
-        <p class="pb-4">Dentist appointments made easy</p>
+      <h1>The Dentist Appointment App</h1>
+      <p class="pb-4">Dentist appointments made easy</p>
     </section>
     <div class="pt-2" id="card-content">
       <div class="row">
         <div class="card mx-auto">
-          <div class="card-body d-flex flex-column">
-            <h3 class="card-title">Are you looking for a dentist?</h3>
-            <p class="card-text">Click on Register to create a free account.</p>
-            <router-link to="/register" class="btn mt-auto">Register</router-link>
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img
+                src="../assets/patient.png"
+                class="user-style-img"
+                alt="patient"
+              />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body d-flex flex-column">
+                <h3 class="card-title">Are you looking for a dentist?</h3>
+                <p class="card-text">
+                  Click on Register to create a free account.
+                </p>
+                <router-link to="/register" class="btn mt-auto"
+                  >Register</router-link
+                >
+              </div>
+            </div>
           </div>
         </div>
         <div class="card mx-auto">
-          <div class="card-body d-flex flex-column">
-            <h3 class="card-title">Are you a dentist?</h3>
-            <p class="card-text">Click on Register to create a free account.</p>
-            <router-link to="/register" class="btn mt-auto">Register</router-link>
+            <div class="row g-0">
+            <div class="col-md-4">
+              <img
+                src="../assets/dental-care.png"
+                class="user-style-img"
+                alt="dentist"
+              />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body d-flex flex-column">
+                <h3 class="card-title">Are you a dentist?</h3>
+                <p class="card-text">
+                  Click on Register to create a free account.
+                </p>
+                <router-link to="/register" class="btn mt-auto"
+                  >Register</router-link
+                >
+              </div>
+            </div>
           </div>
         </div>
+        </div>
       </div>
-    </div>
   </main>
 </template>
 
 <style scoped>
-main{
-  margin-top:30px;
+#home {
+  padding-bottom: 40px;
 }
 .card {
-  width: 20rem;
-  height: 250px;
-  margin-bottom:15px;
-  border-color: #0292f8;
+  width: 25rem;
+  margin-bottom: 15px;
+  border-color: #0292f8; 
 }
-#card-content{
-  margin-bottom:50px;
+
+.user-style-img{
+  margin-top:40px;
+  
 }
-.btn{
-  background-color:#0292f8;
-  color:#fff;
+#card-content {
+  margin-bottom: 80px;
+}
+.btn {
+  background-color: #0292f8;
+  color: #fff;
 }
 </style>
 
