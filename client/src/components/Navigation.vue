7cd57df70dc2d4a92705a778cc65fca34cfd5234
Ivaylo Banchevski <ivaylo.banchevski@gmail.com>
2021-08-13 19:51:27 +0300

    Fix navabar responsiveness
    Make the hamburger menu clickable

--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -1,16 +1,28 @@
 <template>
   <header class="container">
-    <nav class="navbar navbar-expand navbar-dark fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
       <div class="container">
-        
-        <router-link to="/" class=" navbar-brand nav-link">DentApp</router-link>
-        <div class="collapse navbar-collapse">
+        <router-link to="/" class="navbar-brand nav-link">DentApp</router-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          aria-label="Toggle navigation"
+          v-on:click="navCollapsed = !navCollapsed"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="navbar-collapse"
+          v-bind:class="{ collapse: !navCollapsed, collapsed: navCollapsed }"
+        >
           <ul class="navbar-nav">
             <li class="nav-item px-3">
               <router-link to="/" class="nav-link">Home</router-link>
             </li>
             <li class="nav-item px-3">
-              <router-link to="/register" class="nav-link">Register</router-link>
+              <router-link to="/register" class="nav-link"
+                >Register</router-link
+              >
             </li>
             <li class="nav-item px-3">
               <router-link to="/login" class="nav-link">Login</router-link>
@@ -23,11 +35,22 @@
 </template>
 
 <style scoped>
-  nav{
-    background-color: #0292f8;
-  }  
+nav {
+  background-color: #0292f8;
+}
 </style>
 
 <script>
-
+export default {
+  data() {
+    return {
+      navCollapsed: false,
+    };
+  },
+  watch: {
+    $route() {
+      this.navCollapsed = false;
+    },
+  },
+};
 </script>
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,111 +1,121 @@
 <template>
-  <div class="row mt-4" v-if="error">
-    <div class="col-6 offset-md-3">
-      <div class="alert alert-danger">{{error}}</div>
+  <div class="container">
+    <div class="row mt-4" v-if="error">
+      <div class="col-md-6 offset-md-3">
+        <div class="alert alert-danger">{{ error }}</div>
+      </div>
     </div>
-  </div>
-  <div id="login-page" class="container mt-5 shadow">
-    <i class="fas fa-user-lock fa-3x mt-2"></i>
-    <h3 class="mt-3 mb-4">Login</h3>
-    <form @submit.prevent="onLoginSubmit">
-      <div class="form-group mx-auto">
-        <div class="input-group mb-3">
-          <i class="far fa-envelope fa-3x mt-1"></i>
-        <input
-          type="email"
-          class="form-control mt-2 mb-3"
-          id="email"
-          placeholder="Enter email"
-          required
-          v-model="email"
-        />
-        </div>
-      </div>
+    <div class="row">
+      <div class="col-sm-12 col-md-6 col-lg-4 offset-md-3 offset-lg-4">
+        <div id="login-page" class="container mt-5 shadow">
+          <i class="fas fa-user-lock fa-3x mt-2"></i>
+          <h3 class="mt-3 mb-4">Login</h3>
+          <form @submit.prevent="onLoginSubmit">
+            <div class="form-group mx-auto">
+              <div class="input-group mb-3">
+                <i class="far fa-envelope fa-3x mt-1"></i>
+                <input
+                  type="email"
+                  class="form-control mt-2 mb-3"
+                  id="email"
+                  placeholder="Enter email"
+                  required
+                  v-model="email"
+                />
+              </div>
+            </div>
 
-      <div class="form-group mx-auto">
-        <div class="input-group mb-3">
-          <i class="fas fa-fingerprint fa-3x mt-1"></i>
-        <input
-          type="password"
-          class="form-control mt-2 mb-3"
-          id="password"
-          placeholder="Password"
-          required
-          v-model="password"
-        />
+            <div class="form-group mx-auto">
+              <div class="input-group mb-3">
+                <i class="fas fa-fingerprint fa-3x mt-1"></i>
+                <input
+                  type="password"
+                  class="form-control mt-2 mb-3"
+                  id="password"
+                  placeholder="Password"
+                  required
+                  v-model="password"
+                />
+              </div>
+            </div>
+            <button type="submit" class="btn mt-3 mb-2">Login</button>
+          </form>
         </div>
       </div>
-      <button type="submit" class="btn mt-3 mb-2">Login</button>
-    </form>
+    </div>
   </div>
 </template>
 
 <style scoped>
-#login-page{
-  padding-top:10px;
-  padding-bottom:10px;
+#login-page {
+  padding-top: 10px;
+  padding-bottom: 10px;
   margin-bottom: 75px;
   background-color: #fff;
-  width:400px;
+  /* width: 400px; */
   border-top: 4px solid #0292f8;
-  border-radius:4%;
+  border-radius: 4%;
 }
-.far{
-  color:#0292f8;
-  margin-right:10px;
-  margin-left:10px;
+.far {
+  color: #0292f8;
+  margin-right: 10px;
+  margin-left: 10px;
 }
-.fas{
-  color:#0292f8;
-  margin-right:10px;
-  margin-left:10px;
+.fas {
+  color: #0292f8;
+  margin-right: 10px;
+  margin-left: 10px;
 }
-#email{
-  margin-right:15px;
+#email {
+  margin-right: 15px;
 }
-#password{
-  margin-right:15px;
+#password {
+  margin-right: 15px;
 }
-button{
+button {
   background-color: #0292f8;
-  color:#fff;
-  width:50%;
+  color: #fff;
+  width: 50%;
 }
-
 </style>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'Login',
+  name: "Login",
   data() {
     return {
-      error: '',
-      email: '',
-      password: '',
-    }
+      error: "",
+      email: "",
+      password: "",
+    };
   },
   methods: {
     onLoginSubmit() {
       axios
-        .post('/api/v1/accounts/login', { email: this.email, password: this.password })
-        .then(res => {
-          const token = res.data.token;
-          document.cookie = `_token=${token};samesite=lax`;
-          console.log(token);
-        }, error => {
-          if (error.response) {
-            console.log(error.response);
-            this.error = error.response.data.statusmsg;
-          } else if (error.request) {
-            console.log(error.request);
-          } else {
-            console.log(error);
+        .post("/api/v1/accounts/login", {
+          email: this.email,
+          password: this.password,
+        })
+        .then(
+          (res) => {
+            const token = res.data.token;
+            document.cookie = `_token=${token};samesite=lax`;
+            console.log(token);
+          },
+          (error) => {
+            if (error.response) {
+              console.log(error.response);
+              this.error = error.response.data.statusmsg;
+            } else if (error.request) {
+              console.log(error.request);
+            } else {
+              console.log(error);
+            }
           }
-        });
-    }
-  }
-}
-
+        );
+    },
+  },
+};
 </script>
