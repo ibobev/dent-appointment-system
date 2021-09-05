df74b1eb6fea603dd87e5bf7699bdf76d54de26d
Ivan Bobev <bobev.ivan99@gmail.com>
2021-09-05 13:38:37 +0300

    Dentist Dashboard Navigation height fix

--- client/src/views/Dentist/DentistDashboard.vue
+++ client/src/views/Dentist/DentistDashboard.vue
@@ -2,7 +2,7 @@
   <div class="container-fluid patient-dashboard-container p-0">
     <div class="row h-100 m-0">
       <!-- Navigation -->
-      <div class="col-sm-12 col-md-2 m-0 p-0">
+      <div class="col-sm-12 col-md-2 m-0 p-0 dentist-nav">
         <NavigationDentist />
       </div>
       <!-- Content -->
@@ -14,6 +14,12 @@
 </template>
 
 <style scoped>
+@media (min-width: 991px) and (max-width: 2600px)
+{
+  .dentist-nav{
+  min-height: 100vh;
+  }
+}
 
 </style>
 
